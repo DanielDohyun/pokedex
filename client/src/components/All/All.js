8d42a84363c0797e0bb4ac6935eb5f2d59fb76d2
Daniel Kim <kkdh1109@gmail.com>
2021-05-06 20:53:37 -0400

    add searchbox and select options

--- client/src/components/All/All.js
+++ client/src/components/All/All.js
@@ -3,22 +3,62 @@ import axios from 'axios';
 
 function All() {
     const [pokemon, setPokemon] = useState([]);
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+
+    const nameFilter = (e) => {
+        setName(e.target.value);
+        console.log(name)
+    };
+
+    const typeFilter = (e) => {
+        setType(e.target.value);
+        console.log(type)
+    };
 
     useEffect(() => {
         axios
             .get('http://localhost:5000/')
             .then(res => {
                 setPokemon(res.data);
-                console.log(res.data);
+                // console.log(res.data);
         })
             .catch(err => {
                 console.log(err);
         })
     }, [])
 
+
     return (
         <div className='all'>
+            <label htmlFor="name">Name: </label>
+            <input onChange={e => nameFilter(e)} name='name' type="text"/>
+
+            <label htmlFor="types">Type: </label>
+            <select onChange={e => typeFilter(e)} name="types" id="types">
+                <option value="Grass">Grass</option>
+                <option value="Poison">Poison</option>
+                <option value="Fire">Fire</option>
+                <option value="Flying">Flying</option>
+                <option value="Water">Water</option>
+                <option value="Bug">Bug</option>
+                <option value="Normal">Normal</option>
+                <option value="Electric">Electric</option>
+                <option value="Ground">Ground</option>
+                <option value="Fairy">Fairy</option>
+                <option value="Fighting">Fighting</option>
+                <option value="Psychic">Psychic</option>
+                <option value="Rock">Rock</option>
+                <option value="Steel">Steel</option>
+                <option value="Ghost">Ghost</option>
+                <option value="Ice">Ice</option>
+                <option value="Dragon">Dragon</option>
+                <option value="Dark">Dark</option>
+            </select>
+            
             <table>
+                <tbody>
+
                 <tr>
                     <th>#</th>
                     <th>Name</th>
@@ -32,13 +72,16 @@ function All() {
                 </tr>
 
                 {
-                    pokemon && pokemon.map(item => (
-                        <tr>
-                            <td>{ item.id}</td>
+                        pokemon && pokemon.map((item, id) => (
+                        <tr key={id}>
+                                <td>
+                                    {/* <img src={`http://img.pokemondb.net/artwork/${item.name.toString().toLowerCase()}.jpg`}/> */}
+                                {item.id}
+                            </td>
                             <td>{item.name.english}</td>
                             {
                                 item.type.length > 1 ?
-                                <div>
+                                <div className='all_multipleType'>
                                     <td>
                                     {item.type[0]}
                                 </td>
@@ -51,15 +94,16 @@ function All() {
                                 </td>
                                 
                             }
-                            <td>{ item.base.HP}</td>
-                            <td>{ item.base.Attack}</td>
-                            <td>{ item.base.Defense}</td>
-                            {/* <td>{ item.base.Sp. Attack}</td>
-                            <td>{ item.base.Sp. Defense}</td> */}
-                            <td>{ item.base.Speed}</td>
+                            <td>{item.base.HP}</td>
+                            <td>{item.base.Attack}</td>
+                            <td>{item.base.Defense}</td>
+                            {/* <td>{item.base.Sp. Attack}</td>
+                            <td>{item.base.Sp. Defense}</td> */}
+                            <td>{item.base.Speed}</td>
                         </tr>
                     ))
-                }
+                    }
+                </tbody>    
             </table>
         </div>
     )
